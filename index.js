98cee82822457505c29fdf4213c998487c7ab137
İbrahim Şimşek <ibrahim.simsek772@gmail.com>
2026-07-11 11:39:59 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// var randomSuperhero = require("superheroes");
+// var superHeroes =randomSuperhero();
+
+
+
+import { randomSuperhero } from 'superheroes';
+
+
+
+console.log(
+randomSuperhero()
+)
